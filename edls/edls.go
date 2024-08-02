4186b9a80686d6b16b251dae9772911c208a18ee
Luis Octavio <octaviodevtech@gmail.com>
2024-08-02 15:15:17 -0600

    ADD: Constantes para el proyecto final

---
+++ edls/edls.go
@@ -0,0 +1,26 @@
+package main
+
+// FILE TYPE
+const (
+	fileRegula int = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+// FILE EXTENSION
+
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz  = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	jpeg = "jpeg"
+	gif = ".gif"
+)
